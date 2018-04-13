1f9540c9f488094e3098679931a4da53cc7fbc67
stewue <stefan@wuersten.ch>
2018-04-13 07:54:54 -0400

    Always unlock accounts

--- frontend/js/web3-interface.js
+++ frontend/js/web3-interface.js
@@ -5,6 +5,7 @@ Web3InterfaceToServer = {
 	contractAddress : null,
 	contract : null,
 	gas : 0,
+	password : "",
 	
 	init : function (){
 		this.web3 = new Web3( new Web3.providers.HttpProvider( this.url ) );
@@ -30,82 +31,88 @@ Web3InterfaceToServer = {
 	// create new game object in contract
 	createGame : function ( callback ){
 		Frontend.waiting.start();
+		
+		this.unlockAllAccounts( function(){
+			Web3InterfaceToServer.contract.methods.create( 
+					Frontend.players, 
+					Web3InterfaceToServer.web3.utils.toWei( Frontend.deposit.toString(), 'Ether') 
+				)
+				.send({ 'from' : Frontend.players[0], 'value' : Web3InterfaceToServer.web3.utils.toWei( Frontend.deposit.toString(), 'Ether'), 'gas' : Web3InterfaceToServer.gas })
+				.then( function( result, error ){ 
 
-		this.contract.methods.create( 
-				Frontend.players, 
-				this.web3.utils.toWei( Frontend.deposit.toString(), 'Ether') 
-			)
-			.send({ 'from' : Frontend.players[0], 'value' : this.web3.utils.toWei( Frontend.deposit.toString(), 'Ether'), 'gas' : this.gas })
-			.then( function( result, error ){ 
-				
-			
-				Frontend.gameId = result.events.GameObject.returnValues['gameId'];
-			
-				console.log('----------');
-				console.log('createGame:');
-				console.log(error);
-				console.log(result);
-				console.log('GameId is:' + Frontend.gameId );
-				console.log('----------');
+					Frontend.gameId = result.events.GameObject.returnValues['gameId'];
 
-				Web3InterfaceToServer.secondPlayerPay( callback );	
+					console.log('----------');
+					console.log('createGame:');
+					console.log(error);
+					console.log(result);
+					console.log('GameId is:' + Frontend.gameId );
+					console.log('----------');
+
+					Web3InterfaceToServer.secondPlayerPay( callback );	
+				});
 			});
 	},
 	
 	// helper function for create game
 	secondPlayerPay : function ( callback ){
-		this.contract.methods.deposit( Frontend.gameId )
-			.send({ 'from' : Frontend.players[1], 'value' : this.web3.utils.toWei( Frontend.deposit.toString(), 'Ether'), 'gas' : this.gas })
-			.then( function( result, error ){  
-				console.log('----------');
-				console.log('secondPlayerPay:');
-				console.log(error);
-				console.log(result);
-				console.log('----------');
-			
-				Web3InterfaceToServer.checkIfPaid( callback );
-			});
-		
+		this.unlockAllAccounts( function(){
+			Web3InterfaceToServer.contract.methods.deposit( Frontend.gameId )
+				.send({ 'from' : Frontend.players[1], 'value' : Web3InterfaceToServer.web3.utils.toWei( Frontend.deposit.toString(), 'Ether'), 'gas' : Web3InterfaceToServer.gas })
+				.then( function( result, error ){  
+					console.log('----------');
+					console.log('secondPlayerPay:');
+					console.log(error);
+					console.log(result);
+					console.log('----------');
+
+					Web3InterfaceToServer.checkIfPaid( callback );
+				});
+		});
 	},
 	
 	// helper function for create game
 	checkIfPaid : function ( callback ){
-		this.contract.methods.haveBothPaid( Frontend.gameId )
-			.send({ 'from' : Frontend.game.getCurrentPlayerAddress(), 'gas' : this.gas })
-			.then( function( result, error ){  
-				Frontend.waiting.stop(); 
-			
-				console.log('----------');
-				console.log('checkIfPaid:');
-				console.log(error);
-				console.log(result);
-				console.log('----------');
-			
-				if( result ){
-					callback(); 
-				}
-				else {
-					alert('At least one player has not paid the deposit');
-				}
-			});
+		this.unlockAllAccounts( function(){
+			Web3InterfaceToServer.contract.methods.haveBothPaid( Frontend.gameId )
+				.send({ 'from' : Frontend.game.getCurrentPlayerAddress(), 'gas' : Web3InterfaceToServer.gas })
+				.then( function( result, error ){  
+					Frontend.waiting.stop(); 
+
+					console.log('----------');
+					console.log('checkIfPaid:');
+					console.log(error);
+					console.log(result);
+					console.log('----------');
+
+					if( result ){
+						callback(); 
+					}
+					else {
+						alert('At least one player has not paid the deposit');
+					}
+				});
+		});
 	},
 	
 	// make a turn
 	makeTurn : function ( fieldId, callback ){
 		Frontend.waiting.start();
 		
-		this.contract.methods.play( Frontend.gameId, fieldId )
-			.send({ 'from' : Frontend.game.getCurrentPlayerAddress(), 'gas' : this.gas })
-			.then( function( result, error ){ 
-				console.log('----------');
-				console.log('makeTurn (fieldId->' + fieldId + '):');
-				console.log(error);
-				console.log(result);
-				console.log('----------');
-			
-				Frontend.waiting.stop(); 
-				callback(); 
-			});
+		this.unlockAllAccounts( function(){
+			Web3InterfaceToServer.contract.methods.play( Frontend.gameId, fieldId )
+				.send({ 'from' : Frontend.game.getCurrentPlayerAddress(), 'gas' : Web3InterfaceToServer.gas })
+				.then( function( result, error ){ 
+					console.log('----------');
+					console.log('makeTurn (fieldId->' + fieldId + '):');
+					console.log(error);
+					console.log(result);
+					console.log('----------');
+
+					Frontend.waiting.stop(); 
+					callback(); 
+				});
+		});
 	},
 	
 	// get the balance of an account
@@ -133,5 +140,23 @@ Web3InterfaceToServer = {
 
 				callback( result ); 
 			});
+	},
+	
+	unlockAllAccounts : function ( callback ){
+		var total = 0;
+		var counter = 0;
+		this.web3.eth.getAccounts().then(function( result, error ){ 
+			total = result.length;
+			for( var i = 0; i < result.length; i++ ){
+				Web3InterfaceToServer.web3.eth.personal.unlockAccount( result[i], Web3InterfaceToServer.password, 9999 ).then(function( result, error ){ 
+					counter++;
+					
+					if( counter == total ){
+						console.log( 'Unlock all accounts' );
+						callback();
+					}
+				});
+			}
+		});
 	}
 };
